1d8c24bb31b08d82ce3328548267830e89aee67e
jackeline Gonzales B <jackiegb@jackelines-Mini.attlocal.net>
2022-03-19 22:37:24 -0400

    commit

--- local_settings_copy.py
+++
@@ -1,6 +0,0 @@
-postgresql = {'pguser':'postgres',
-              'pgpasswd':'1959-Lima',
-              'pghost': 'localhost',
-              'pgport': 5432,
-              'pgdb': 'Project_Success_01',
-              }
