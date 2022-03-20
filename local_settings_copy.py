c0a7a1e063f4f2e20b47f2ce4a6c000cd4d2edb4
jackeline Gonzales B <jackiegb@jackelines-Mini.attlocal.net>
2022-03-19 22:35:24 -0400

    gitignore

---
+++ local_settings_copy.py
@@ -0,0 +1,6 @@
+postgresql = {'pguser':'postgres',
+              'pgpasswd':'1959-Lima',
+              'pghost': 'localhost',
+              'pgport': 5432,
+              'pgdb': 'Project_Success_01',
+              }
